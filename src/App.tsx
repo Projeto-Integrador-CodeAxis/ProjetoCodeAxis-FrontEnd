da558494ff034b711147898c42fa5df95a6a4f63
adriel <adriel.martinelli@gmail.com>
2024-05-21 14:03:33 -0300

    sobre

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
-
+import Sobre from './pages/sobre/Sobre'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+        <Route path="/Sobre" element={<Sobre />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
