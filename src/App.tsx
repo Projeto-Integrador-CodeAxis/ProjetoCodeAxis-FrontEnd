64aace32bd9ec5d408b847706e34c81b44f47239
JhulienyB <jhulienybucci@gmail.com>
2024-05-21 11:23:49 -0300

    Criando Projeto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
