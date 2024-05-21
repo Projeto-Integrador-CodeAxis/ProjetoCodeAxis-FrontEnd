15f5d62b1ecc67916539b848900409d3deb6ca28
michel <michelc.desouza@gmail.com>
2024-05-21 15:32:31 -0300

    task_08_home

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './App.css'
 import './index.css'
+import Home from './pages/home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className='gradient h-screen w-screen'>
+        <Home />
+      </div>
     </>
   )
 }
