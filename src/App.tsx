90f60911f16eaa38a61214398508f0934371c136
Jessica <jessica_costaignacio@hotmail.com>
2024-05-21 15:01:03 -0300

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './index.css'
+import Footer from './components/pages/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    
+       <Footer />
+
     </>
   )
 }
