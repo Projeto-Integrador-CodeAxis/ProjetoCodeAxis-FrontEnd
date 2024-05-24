2f6be1636439c97ea7b7974f9b3d01f741d2b2c4
michel <michelc.desouza@gmail.com>
2024-05-24 10:18:39 -0300

    att task 09

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,23 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/home/Home'
 import Footer from './components/footer/Footer'
 import Navbar from "./components/navBar/NavBar";
-import Contatos from './pages/pg_contatos/Contatos'
+import Contatos from './pages/contatos/Contatos'
 
 function App() {
 
   return (
     <>
       <div className='gradient h-screen w-screen'>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/" element={<Home />} />
-         <Route path="/contatos" element={<Contatos />} />
-        </Routes>
-      <Footer />
-      </BrowserRouter>
+        
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/contatos" element={<Contatos />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
       
       </div>
     </>
