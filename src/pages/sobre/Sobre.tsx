a4712058964efdc793a9eb2c158371769a497939
michel <michelc.desouza@gmail.com>
2024-06-05 19:58:28 -0300

    feat: card about att

--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -38,146 +38,207 @@ function Sobre(){
        </div>
 
        <div className="flex justify-center min-h-[0vh]">
-        <div className=" container grid grid-cols-3 flex-row align-middle text-white justify-items-center mb-20  ">
-            
-            <div className="max-w-7xl flex flex-col size-96 items-center justify-center py-4 pb-20">
-                <h1 className="text-xl pb-5">Adriel Modesto Martineli</h1>
-                <img src="https://avatars.githubusercontent.com/u/136142475?v=4" 
-                alt="Imagem da pagina home"
-                className="border-solid border-white  border-4 rounded-full w-2/3" />
-                <div className='flex p-4 gap-2'>
-                <a href="https://www.linkedin.com/in/adriel-martineli-57a0551b7/" target='_blank'>
-                <LinkedinLogo size={40} />
-                </a>
-                <a href="https://github.com/AdrielMartineli" target='_blank'>
-                <GithubLogo size={40} />
-                </a>
+        <div className=" container grid grid-cols-3 flex-row align-middle text-white justify-items-center mt-20  ">
+
+                <div className='flex items-center mb-40'>
+                    <div className='flex flex-col items-center bg-celestial-blue w-96 rounded-2xl shadow'>
+                        <div className='mt-[-120px]'>
+                        <img src="https://avatars.githubusercontent.com/u/136142475?v=4"
+                        alt=""
+                        className="rounded-full w-48 border-white border-4" />
+                        </div>
+                        <div className='mt-4'>
+                            <h1 className='font-bold text-2xl'>Adriel Modesto Martineli</h1>
+                            
+                        </div>
+                        <div className='flex p-4 gap-2'>
+                            <a href="https://www.linkedin.com/in/adriel-martineli-57a0551b7/" target='_blank'>
+                            <LinkedinLogo size={40} />
+                            </a>
+                            <a href="https://github.com/AdrielMartineli" target='_blank'>
+                            <GithubLogo size={40} />
+                        </a>
+                        </div>
+                    </div>
                 </div>
                 
-            </div>  
-            <div className="max-w-7xl flex  flex-col size-96 items-center justify-center py-4 pb-20">
-                    <h1 className="text-xl pb-5">Ana Clara De Alencar</h1>
-                <img src="https://avatars.githubusercontent.com/u/162627715?v=4" 
-                alt="Imagem da pagina home"
-                className="border-solid border-white  border-4 rounded-full w-2/3" />
-                <div className='flex p-4 gap-2'>
-                <a href="https://www.linkedin.com/in/anaclara-de-alencar/" target='_blank'>
-                <LinkedinLogo size={40} />
-                </a>
-                <a href="https://github.com/anaclaraalencar" target='_blank'>
-                <GithubLogo size={40} />
-                </a>
+                <div className='flex items-center mb-40'>
+                    <div className='flex flex-col items-center bg-celestial-blue w-96 rounded-2xl shadow'>
+                        <div className='mt-[-120px]'>
+                        <img src="https://avatars.githubusercontent.com/u/162627715?v=4"
+                        alt=""
+                        className="rounded-full w-48 border-white border-4" />
+                        </div>
+                        <div className='mt-4'>
+                            <h1 className='font-bold text-2xl'>Ana Clara De Alencar</h1>
+                            
+                        </div>
+                        <div className='flex p-4 gap-2'>
+                            <a href="https://www.linkedin.com/in/anaclara-de-alencar/" target='_blank'>
+                            <LinkedinLogo size={40} />
+                            </a>
+                            <a href="https://github.com/anaclaraalencar" target='_blank'>
+                            <GithubLogo size={40} />
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </div> 
-
-            <div className="max-w-7xl flex  flex-col size-96 items-center justify-center py-4 pb-20">
-                    <h1 className="text-xl pb-5">André Luiz Nascimento Pinto</h1>
-                <img src="https://avatars.githubusercontent.com/u/162219194?v=4" 
-                alt="Imagem da pagina home"
-                className="border-solid border-white border-4 rounded-full w-2/3" />
-                <div className='flex p-4 gap-2'>
-                <a href="https://www.linkedin.com/in/https://www.linkedin.com/in/andrenas95//" target='_blank'>
-                <LinkedinLogo size={40} />
-                </a>
-                <a href="https://github.com/andrenas95" target='_blank'>
-                <GithubLogo size={40} />
-                </a>
+                
+                <div className='flex items-center mb-40'>
+                    <div className='flex flex-col items-center bg-celestial-blue w-96 rounded-2xl shadow'>
+                        <div className='mt-[-120px]'>
+                        <img src="https://avatars.githubusercontent.com/u/162219194?v=4"
+                        alt=""
+                        className="rounded-full w-48 border-white border-4" />
+                        </div>
+                        <div className='mt-4'>
+                            <h1 className='font-bold text-2xl'>André Luiz Nascimento Pinto</h1>
+                            
+                        </div>
+                        <div className='flex p-4 gap-2'>
+                            <a href="https://www.linkedin.com/in/https://www.linkedin.com/in/andrenas95//" target='_blank'>
+                            <LinkedinLogo size={40} />
+                            </a>
+                            <a href="https://github.com/andrenas95" target='_blank'>
+                            <GithubLogo size={40} />
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </div> 
             
-            <div className="max-w-7xl flex  flex-col size-96 items-center justify-center py-8">
-                <h1 className="text-xl pb-5"> Daniele Campos Ferreira</h1>
-                <img src="https://avatars.githubusercontent.com/u/131809585?v=4" 
-                alt="Imagem da pagina home"
-                className="border-solid border-white  border-4 rounded-full w-2/3" />
-                <div className='flex p-4 gap-2'>
-                <a href="https://www.linkedin.com/in/daniele-campos-ferreira/" target='_blank'>
-                <LinkedinLogo size={40} />
-                </a>
-                <a href="https://github.com/daniele0118" target='_blank'>
-                <GithubLogo size={40} />
-                </a>
+                <div className='flex items-center mb-40'>
+                    <div className='flex flex-col items-center bg-celestial-blue w-96 rounded-2xl shadow'>
+                        <div className='mt-[-120px]'>
+                        <img src="https://avatars.githubusercontent.com/u/131809585?v=4"
+                        alt=""
+                        className="rounded-full w-48 border-white border-4" />
+                        </div>
+                        <div className='mt-4'>
+                            <h1 className='font-bold text-2xl'>Daniele Campos Ferreira</h1>
+                            
+                        </div>
+                        <div className='flex p-4 gap-2'>
+                            <a href="https://www.linkedin.com/in/daniele-campos-ferreira/" target='_blank'>
+                            <LinkedinLogo size={40} />
+                            </a>
+                            <a href="https://github.com/daniele0118" target='_blank'>
+                            <GithubLogo size={40} />
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </div>
+                
+                <div className='flex items-center mb-40'>
+                    <div className='flex flex-col items-center bg-celestial-blue w-96 rounded-2xl shadow'>
+                        <div className='mt-[-120px]'>
+                        <img src="https://avatars.githubusercontent.com/u/121838715?v=4"
+                        alt=""
+                        className="rounded-full w-48 border-white border-4" />
+                        </div>
+                        <div className='mt-4'>
+                            <h1 className='font-bold text-2xl'>Igor Fernandes Quaresma</h1>
+                            
+                        </div>
+                        <div className='flex p-4 gap-2'>
+                            <a href="https://www.linkedin.com/in/ifernandesq/" target='_blank'>
+                            <LinkedinLogo size={40} />
+                            </a>
+                            <a href="https://github.com/IgorFernandesQuaresma" target='_blank'>
+                            <GithubLogo size={40} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
 
-            <div className="max-w-7xl flex  flex-col  size-96 items-center justify-center py-8">
-                <h1 className="text-xl pb-5">Igor Fernandes Quaresma</h1>
-                <img src="https://avatars.githubusercontent.com/u/121838715?v=4" 
-                alt="Imagem da pagina home"
-                className="border-solid border-white  border-4 rounded-full w-2/3" />
-                <div className='flex p-4 gap-2'>
-                <a href="https://www.linkedin.com/in/ifernandesq/" target='_blank'>
-                <LinkedinLogo size={40} />
-                </a>
-                <a href="https://github.com/IgorFernandesQuaresma" target='_blank'>
-                <GithubLogo size={40} />
-                </a>
+                <div className='flex items-center mb-40'>
+                    <div className='flex flex-col items-center bg-celestial-blue w-96 rounded-2xl shadow'>
+                        <div className='mt-[-120px]'>
+                        <img src="https://avatars.githubusercontent.com/u/120491040?v=4"
+                        alt=""
+                        className="rounded-full w-48 border-white border-4" />
+                        </div>
+                        <div className='mt-4'>
+                            <h1 className='font-bold text-2xl'>Jessica Costa Ignácio</h1>
+                            
+                        </div>
+                        <div className='flex p-4 gap-2'>
+                            <a href="https://www.linkedin.com/in/jessicaignacio/" target='_blank'>
+                            <LinkedinLogo size={40} />
+                            </a>
+                            <a href="https://github.com/eijess" target='_blank'>
+                            <GithubLogo size={40} />
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </div>
 
-            <div className="max-w-7xl flex  flex-col size-96 items-center justify-center py-8">
-                <h1 className="text-xl pb-5">Jessica Costa Ignácio</h1>
-                <img src="https://avatars.githubusercontent.com/u/120491040?v=4" 
-                alt="Imagem da pagina home"
-                className="border-solid border-white  border-4 rounded-full w-2/3" />
-                <div className='flex p-4 gap-2'>
-                <a href="https://www.linkedin.com/in/jessicaignacio/" target='_blank'>
-                <LinkedinLogo size={40} />
-                </a>
-                <a href="https://github.com/eijess" target='_blank'>
-                <GithubLogo size={40} />
-                </a>
+                <div className='flex items-center mb-40'>
+                    <div className='flex flex-col items-center bg-celestial-blue w-96 rounded-2xl shadow'>
+                        <div className='mt-[-120px]'>
+                        <img src="https://avatars.githubusercontent.com/u/117844737?v=4"
+                        alt=""
+                        className="rounded-full w-48 border-white border-4" />
+                        </div>
+                        <div className='mt-4'>
+                            <h1 className='font-bold text-2xl'>Jhulieny Bucci</h1>
+                            
+                        </div>
+                        <div className='flex p-4 gap-2'>
+                            <a href="https://www.linkedin.com/in/jhulieny-bucci-72ba55232/" target='_blank'>
+                            <LinkedinLogo size={40} />
+                            </a>
+                            <a href="https://github.com/JhulienyB" target='_blank'>
+                            <GithubLogo size={40} />
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </div>
 
-            <div className="max-w-7xl flex  flex-col size-96 items-center justify-center py-4 pt-20">
-                <h1 className="text-xl pb-5">Jhulieny Bucci</h1>
-                <img src="https://avatars.githubusercontent.com/u/117844737?v=4" 
-                alt="Imagem da pagina home"
-                className="border-solid border-white border-4 rounded-full w-2/3" />
-                <div className='flex p-4 gap-2'>
-                <a href="https://www.linkedin.com/in/jhulieny-bucci-72ba55232/" target='_blank'>
-                <LinkedinLogo size={40} />
-                </a>
-                <a href="https://github.com/JhulienyB" target='_blank'>
-                <GithubLogo size={40} />
-                </a>
+                <div className='flex items-center mb-40'>
+                    <div className='flex flex-col items-center bg-celestial-blue w-96 rounded-2xl shadow'>
+                        <div className='mt-[-120px]'>
+                        <img src="https://avatars.githubusercontent.com/u/149630140?v=4"
+                        alt=""
+                        className="rounded-full w-48 border-white border-4" />
+                        </div>
+                        <div className='mt-4'>
+                            <h1 className='font-bold text-2xl text-center'>Michel Cavalcante</h1>
+                            
+                        </div>
+                        <div className='flex p-4 gap-2'>
+                            <a href="https://www.linkedin.com/in/dev-cavalcante/" target='_blank'>
+                            <LinkedinLogo size={40} />
+                            </a>
+                            <a href="https://github.com/mcavalcantedesouza" target='_blank'>
+                            <GithubLogo size={40} />
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </div>
 
-            <div className="max-w-7xl flex  flex-col size-96 items-center justify-center py-4 pt-20">
-                <h1 className="text-xl pb-5">Michel Oliveira Cavalcante de Souza</h1>
-                <img src="https://avatars.githubusercontent.com/u/149630140?v=4" 
-                alt="Imagem da pagina home"
-                className="border-solid border-white border-4 rounded-full w-2/3" />
-                <div className='flex p-4 gap-2'>
-                <a href="https://www.linkedin.com/in/dev-cavalcante/" target='_blank'>
-                <LinkedinLogo size={40} />
-                </a>
-                <a href="https://github.com/mcavalcantedesouza" target='_blank'>
-                <GithubLogo size={40} />
-                </a>
-                </div>
-            </div>
-
-            <div className="max-w-7xl flex  flex-col size-96 items-center justify-center py-4 pt-20">
-                <h1 className="text-xl pb-5">Mariah Caroline P. G. Oliveira</h1>
-                <img src="https://avatars.githubusercontent.com/u/162521119?v=4" 
-                alt="Imagem da pagina home"
-                className="border-solid border-white  border-4 rounded-full w-2/3" />
-                <div className='flex p-4 gap-2'>
-                <a href="https://www.linkedin.com/in/a-mariah-garcia/" target='_blank'>
-                <LinkedinLogo size={40} />
-                </a>
-                <a href="https://github.com/Mariahcpgo" target='_blank'>
-                <GithubLogo size={40} />
-                </a>
-                </div>
-            </div>
-            
+                <div className='flex items-center mb-40'>
+                    <div className='flex flex-col items-center bg-celestial-blue w-96 rounded-2xl shadow'>
+                        <div className='mt-[-120px]'>
+                        <img src="https://avatars.githubusercontent.com/u/162521119?v=4"
+                        alt=""
+                        className="rounded-full w-48 border-white border-4" />
+                        </div>
+                        <div className='mt-4'>
+                            <h1 className='font-bold text-2xl text-center'>Mariah Caroline P. G. Oliveira</h1>
+                            
+                        </div>
+                        <div className='flex p-4 gap-2'>
+                            <a href="https://www.linkedin.com/in/a-mariah-garcia/" target='_blank'>
+                            <LinkedinLogo size={40} />
+                            </a>
+                            <a href="https://github.com/Mariahcpgo" target='_blank'>
+                            <GithubLogo size={40} />
+                            </a>
+                        </div>
+                    </div>
+                </div>            
         </div>
        </div>
-       
         </>
     );
 }
